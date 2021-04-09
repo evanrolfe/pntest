16ef1027ddd3dfc50de996542d05ea65f9bed7cb
Evan Rolfe <evanrolfe@gmail.com>
2021-04-09 17:04:57 +0100

    Remove un-used tables from the schema

--- src/lib/database_schema.py
+++ src/lib/database_schema.py
@@ -1,78 +1,10 @@
-NUM_TABLES = 13
-SCHEMA_SQL = """CREATE TABLE IF NOT EXISTS requests(
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  client_id INTEGER,
-  method TEXT,
-  url TEXT,
-  host TEXT,
-  encrypted BOOLEAN,
-  http_version TEXT,
-  path TEXT,
-  ext TEXT,
-  websocket_request_id TEXT,
-  websocket_sec_key TEXT,
-
-  request_modified BOOLEAN,
-  modified_method TEXT,
-  modified_url TEXT,
-  modified_host TEXT,
-  modified_http_version TEXT,
-  modified_path TEXT,
-  modified_ext TEXT,
-  modified_request_headers TEXT,
-  modified_request_payload TEXT,
-
-  request_type TEXT,
-  response_body_rendered TEXT,
-  response_remote_address TEXT,
-  response_http_version TEXT,
-
-  request_headers TEXT,
-  request_payload TEXT,
-  response_status INTEGER,
-  response_status_message TEXT,
-  response_headers TEXT,
-  response_body TEXT,
-  response_body_length INTEGER,
-
-  response_modified BOOLEAN,
-  modified_response_status INTEGER,
-  modified_response_status_message TEXT,
-  modified_response_headers TEXT,
-  modified_response_body TEXT,
-  modified_response_body_length INTEGER,
-  modified_response_http_version TEXT,
-
-  created_at INTEGER,
-  updated_at INTEGER
-);
-
-CREATE TABLE IF NOT EXISTS editor_items(
+NUM_TABLES = 11
+SCHEMA_SQL = """CREATE TABLE IF NOT EXISTS editor_items(
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   parent_id INTEGER,
   name TEXT NOT NULL,
   item_type TEXT NOT NULL,
   item_id INTEGER,
-  created_at INTEGER,
-  updated_at INTEGER
-);
-
-CREATE TABLE IF NOT EXISTS editor_requests(
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  parent_id INTEGER,
-  method TEXT,
-  url TEXT,
-  request_headers TEXT,
-  request_payload TEXT,
-
-  response_remote_address TEXT,
-  response_http_version TEXT,
-  response_status INTEGER,
-  response_status_message TEXT,
-  response_headers TEXT,
-  response_body TEXT,
-  response_body_length INTEGER,
-
   created_at INTEGER,
   updated_at INTEGER
 );
