b6ab3de19b3cf3d20d3460b380c5789b33c4fe37
Evan Rolfe <accounts@evanrolfe.me>
2022-08-21 11:50:38 +0100

    fix test

--- test/unit/lib/text_wrapper_test.py
+++ test/unit/lib/text_wrapper_test.py
@@ -3,34 +3,40 @@ from models.data.variable import Variable
 from support.factories import factory
 
 class TestTextWrapper:
-    # def test_parsing_variable_inside_encoding(self, database, cleanup_database):
-    #     factory(Variable, 'global').create(key='myVar', value='hello')
-    #     factory(Variable, 'global').create(key='otherVar', value='world')
+    def test_parsing_variable_inside_encoding(self, database, cleanup_database):
+        factory(Variable, 'global').create(key='myVar', value='hello')
+        factory(Variable, 'global').create(key='otherVar', value='world')
 
-    #     value = '{"this is var:": "${b64:${var:myVar} - ${var:otherVar}}","asdf": "another one!"}'
-    #     text_wrapper = TextWrapper(value, {})
-    #     result = text_wrapper.get_parsed_text()
+        value = '{"this is var:": "${b64:${var:myVar} - ${var:otherVar}}","asdf": "another one!"}'
+        text_wrapper = TextWrapper(value, {})
+        result = text_wrapper.get_parsed_text()
 
-    #     assert result == '{"this is var:": "aGVsbG8gLSB3b3JsZA==","asdf": "another one!"}'
+        assert result == '{"this is var:": "aGVsbG8gLSB3b3JsZA==","asdf": "another one!"}'
 
-    # def test_finding_the_node_containing_index(self, database, cleanup_database):
-    #     value = '{"this is var:": "${b64:${var:myVar} - ${var:otherVar}}","asdf": "another one!"}'
-    #     text_wrapper = TextWrapper(value, {})
+    def test_finding_the_node_containing_index(self, database, cleanup_database):
+        factory(Variable, 'global').create(key='myVar', value='hello')
+        factory(Variable, 'global').create(key='otherVar', value='world')
 
-    #     var_node = text_wrapper.find_node_containing_index(54)
+        value = '{"this is var:": "${b64:${var:myVar} - ${var:otherVar}}","asdf": "another one!"}'
+        text_wrapper = TextWrapper(value, {})
 
-    #     assert var_node is not None
-    #     assert var_node.sub_str == 'b64:${var:myVar} - ${var:otherVar}'
+        var_node = text_wrapper.find_node_containing_index(54)
 
-    # def test_when_a_node_contains_an_invalid_value(self, database, cleanup_database):
-    #     value = '{"this is var:": "${var:otherVar}","asdf": "${this_is_not_valid}"}'
-    #     text_wrapper = TextWrapper(value, {})
+        assert var_node is not None
+        assert var_node.sub_str == 'b64:${var:myVar} - ${var:otherVar}'
 
-    #     result = text_wrapper.get_parsed_text()
-    #     child_nodes = text_wrapper.get_immediate_children()
+    def test_when_a_node_contains_an_invalid_value(self, database, cleanup_database):
+        factory(Variable, 'global').create(key='myVar', value='hello')
+        factory(Variable, 'global').create(key='otherVar', value='world')
 
-    #     assert result == '{"this is var:": "world","asdf": "${this_is_not_valid}"}'
-    #     assert len(child_nodes) == 1
+        value = '{"this is var:": "${var:otherVar}","asdf": "${this_is_not_valid}"}'
+        text_wrapper = TextWrapper(value, {})
+
+        result = text_wrapper.get_parsed_text()
+        child_nodes = text_wrapper.get_immediate_children()
+
+        assert result == '{"this is var:": "world","asdf": "${this_is_not_valid}"}'
+        assert len(child_nodes) == 1
 
     def test_when2(self, database, cleanup_database):
         value = """{
